a3d66758b8ae2cbc21d3eef1be4e19ab197aeb87
styanr <chaus.oleh@gmail.com>
2023-03-06 21:34:04 +0200

    add adding functionality

---
+++ Lab0/app.js
@@ -0,0 +1,22 @@
+const studentsTable = document.getElementById("students-table");
+const buttonWrapper = document.getElementById("button-wrapper");
+
+function addStudent() {
+    let newRow = studentsTable.insertRow(-1);
+    const cellCount = studentsTable.rows[0].cells.length;
+    for (let i = 0; i < cellCount; i++) {
+        newRow.insertCell(0);
+    }
+    let newInput = document.createElement("input");
+    newInput.type = "checkbox";
+    newRow.cells[0].appendChild(newInput);
+    newRow.cells[1].textContent = "XX-YY";
+    newRow.cells[2].textContent = "Test Name";
+    newRow.cells[3].textContent = "NB";
+    newRow.cells[4].textContent = "01.01.2004";
+    let newIndicator = document.createElement("div");
+    newIndicator.className = "green";
+    newRow.cells[5].appendChild(newIndicator);
+    newRow.cells[6].appendChild(buttonWrapper.cloneNode(true));
+
+}
