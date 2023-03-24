59a6fe2ef3b38eb48ebc5045aade40076688127c
styanr <chaus.oleh@gmail.com>
2023-03-24 18:27:13 +0200

    add form for editing/adding

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+
+class Student {
+    constructor(group, name, surname, gender, birthday, id) {
+        this.group = group;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.id = id;
+    }
+}
+
+const students = [];
+const studentIDs = new Set();
+
 const studentsTable = document.getElementById("students-table");
-const buttonWrapper = document.getElementById("button-wrapper");
 const pages = this.document.getElementById("pages");
 const mainNav = document.getElementById("main-nav");
 
@@ -10,6 +24,7 @@ const pagesStyle = getComputedStyle(pages);
 const bottomPos = pages.offsetHeight - parseFloat(getComputedStyle(pages).getPropertyValue("padding-bottom"));
 console.log(savedPos.bottom);
 let lastScroll = 0;
+
 window.addEventListener("scroll", function () {
     let thisScoll = this.scrollY;
     if (thisScoll > lastScroll) {
@@ -35,81 +50,133 @@ const Buttons = {
     OK: 1,
 }
 
-function createForm(name) {
-    const notificationWindow = createPopup(name, "", []);
-    console.log($(notificationWindow).children("h2"));
-    $(notificationWindow).children("h2").after(`
-    <form>
-      <div class="row my-3 g-0">
-        <label class="form-label col-xl-3 col-form-label" for="group-select">Group</label>
-        <select class="form-select col-xl" id="group-select" required aria-label="Select Group">
-          <option value="" selected disabled>Select Group</option>
-          <option>KN-21</option>
-          <option>PZ-21</option>
-          <option>PZ-22</option>    
-        </select>
-      </div>
-      <div class="row mb-3 g-0">
-        <label class="form-label col-xl-3 col-form-label" for="name-input">Name</label>
-        <input type="text" id="name-input" class="form-control col-xl" required>
-      </div>
-      <div class="row mb-3 g-0">
-        <label class="form-label col-xl-3 col-form-label" for="surname-input">Surname</label>
-        <input type="text" id="surname-input" class="form-control col-xl" required>
-      </div>
-      <div class="row mb-3 g-0">
-        <label class="form-label col-xl-3 col-form-label" for="group-select">Gender</label>
-        <select class="form-select col-xl" id="gender-select" required aria-label="Select Gender">
-          <option value="" selected disabled>Select Gender</option>
-          <option>Male</option>
-          <option>Female</option>
-          <option>Non-Binary</option>    
-        </select>
-      </div>
-      <div class="row mb-3 g-0">
-        <label class="form-label col-xl-3 col-form-label" for="date-select">Birthday</label>
-        <input type="date" id="surname-input" class="form-control col-xl" required>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-
-`);
+function isAssigned(id) {
+    return studentIDs.has(id);
 }
 
+function generateUniqueID() {
+    let id = Math.floor(Math.random() * 100000).toString().padStart(5, "0");
+    while (isAssigned(id)) {
+        id = Math.floor(Math.random() * 100000).toString().padStart(5, "0");
+    }
+    return id;
+}
+
+function createForm(name, id = -1) {
+    id = Number(id);
+    const notificationWindow = createPopup(name, "", []);
+    $(notificationWindow).children("p").remove();
+    $(notificationWindow).children(".buttons").remove();
+    const newEditForm = $("#edit-form").clone();
+    newEditForm.removeAttr("class");
+    $(newEditForm).find(".button-cancel").click(closePopup);
+    $(notificationWindow).children("h2").after(newEditForm);
+
+    let action;
+
+    if (id >= 0) {
+        action = "edit";
+        const thisStudent = students.find((student) => Number(student.id) === Number(id));
+        console.log(thisStudent);
+        newEditForm.find("#group-select").val(thisStudent.group);
+        newEditForm.find("#name-input").val(thisStudent.name);
+        newEditForm.find("#surname-input").val(thisStudent.surname);
+        newEditForm.find("#gender-select").val(thisStudent.gender);
+        newEditForm.find("#date-select").val(thisStudent.birthday);
+        newEditForm.submit(function (e) {
+            e.preventDefault();
+            thisStudent.group = newEditForm.find("#group-select").val();
+            thisStudent.name = newEditForm.find("#name-input").val();
+            thisStudent.surname = newEditForm.find("#surname-input").val();
+            thisStudent.gender = newEditForm.find("#gender-select").val();
+            thisStudent.birthday = newEditForm.find("#date-select").val();
+            console.log(thisStudent);
+
+            const thisRow = $(`tr[data-id=${thisStudent.id}]`);
+            thisRow.find("td:nth-child(2)").text(thisStudent.group);
+            thisRow.find("td:nth-child(3)").text(`${thisStudent.name} ${thisStudent.surname}`);
+            thisRow.find("td:nth-child(4)").text(thisStudent.gender);
+            thisRow.find("td:nth-child(5)").text(thisStudent.birthday);
+            const thisStudentAction = {
+                action: action,
+                student: thisStudent
+            };
+            console.log(JSON.stringify(thisStudentAction));
+            closePopup();
+        });
+    }
+    else {
+        action = "add";
+        newEditForm.submit(function (e) {
+            e.preventDefault();
+            const newRow = $("<tr>");
+            newRow.append($("<td>").append($("<input>").attr("type", "checkbox")));
+            newRow.append($("<td>").text(newEditForm.find("#group-select").val()));
+            newRow.append($("<td>").text(`${newEditForm.find("#name-input").val()} ${newEditForm.find("#surname-input").val()}`));
+            newRow.append($("<td>").text(newEditForm.find("#gender-select").val()));
+            newRow.append($("<td>").text(newEditForm.find("#date-select").val()));
+            newRow.append($("<td>").text());
+            newRow.append($("<td>").append($("<div>").addClass("green-dot")));
+            newRow.append($("<td>").append($("#button-wrapper").clone().removeClass("d-none")));
+            const thisStudent = new Student(newEditForm.find("#group-select").val(),
+                newEditForm.find("#name-input").val(),
+                newEditForm.find("#surname-input").val(),
+                newEditForm.find("#gender-select").val(),
+                newEditForm.find("#date-select").val(),
+                generateUniqueID());
+            students.push(thisStudent);
+            studentIDs.add(thisStudent.id);
+            console.log(thisStudent.id);
+            console.log($("#edit-button"));
+            console.log(newRow);
+            $(newRow).attr("data-id", thisStudent.id);
+            $("#students-table > tbody").append(newRow);
+
+
+            $(newRow).find(".edit-button").click(function () {
+                createForm("Edit student", $(this).parent().parent().parent().data("id"));
+            })
+            $(newRow).find(".delete-button").click(function () {
+                deleteStudent($(this).parent().parent().parent().data("id"));
+            });
+            const thisStudentAction = {
+                action: action,
+                student: thisStudent
+            };
+            console.log(JSON.stringify(thisStudentAction));
+            closePopup();
+        });
+    }
+}
+
+$("#add-button").on("keydown", function (event) {
+    console.log(event);
+    if (event.keyCode === 13) {
+        event.preventDefault();
+    }
+});
 
 $("#add-button").click(function () {
-    // const buttonList = createPopup("Add student", "", [Buttons.OK, Buttons.Cancel]);
-    // buttonList[0].addEventListener("click", function () {
-    //     let newRow = studentsTable.insertRow(-1);
-    //     const cellCount = studentsTable.rows[0].cells.length;
-    //     for (let i = 0; i < cellCount; i++) {
-    //         newRow.insertCell(0);
-    //     }
-    //     let newInput = document.createElement("input");
-    //     newInput.type = "checkbox";
-    //     newRow.cells[0].appendChild(newInput);
-    //     newRow.cells[1].textContent = "XX-YY";
-    //     newRow.cells[2].textContent = "Test Name";
-    //     newRow.cells[3].textContent = "NB";
-    //     newRow.cells[4].textContent = "01.01.2004";
-    //     let newIndicator = document.createElement("div");
-    //     newIndicator.className = "green-dot";
-    //     newRow.cells[5].appendChild(newIndicator);
-    //     newRow.cells[6].appendChild(buttonWrapper.cloneNode(true));
-    // })
     createForm("Add student");
 });
 
-function deleteStudent(element) {
-    const thisRow = element.parentNode.parentNode.parentNode;
-    const thisBody = thisRow.parentNode;
-    const thisName = thisRow.cells[2].textContent;
-    const buttonList = createPopup("Warning", `Are you sure you want to delete user ${thisName} ? `, [Buttons.OK, Buttons.Cancel]);
-    for (const button of buttonList) {
-        console.log(button.role);
-        if (button.role == Buttons.OK) {
+function deleteStudent(id) {
+    console.log(id);
+    const thisStudent = students.find((student) => Number(student.id) === Number(id));
+    const notificationWindow = createPopup("Warning", `Are you sure you want to delete user ${thisStudent.name} ? `, [Buttons.OK, Buttons.Cancel]);
+    console.log(notificationWindow.find("button"));
+    for (const button of notificationWindow.find("button")) {
+        if ($(button).attr("role") == Buttons.OK) {
             button.addEventListener("click", function () {
-                thisBody.removeChild(thisRow);
+                $(`tr[data-id=${id}]`).remove();
+                students.splice(students.findIndex((student) => Number(student.id) === Number(id)));
+                studentIDs.delete(id.toString());
+                let action = "delete";
+                const thisStudentAction = {
+                    action: action,
+                    id: thisStudent.id
+                };
+                console.log(JSON.stringify(thisStudentAction));
             })
         }
     }
@@ -125,43 +192,33 @@ const overlay = document.getElementById("overlay");
 
 
 function createPopup(title, content, buttonRoleList) {
-    const notificationWindow = document.createElement("div");
-    notificationWindow.className = "alerts";
+    const notificationWindow = $("<div>").addClass("alerts");
 
-    const notificationHeading = document.createElement("h2");
-    notificationHeading.textContent = title;
-    notificationWindow.appendChild(notificationHeading);
+    const notificationHeading = $("<h2>").text(title);
+    notificationWindow.append(notificationHeading);
 
-    const notificationContent = document.createElement("p");
-    notificationContent.textContent = content;
-    notificationWindow.appendChild(notificationContent);
+    const notificationContent = $("<p>").text(content);
+    notificationWindow.append(notificationContent);
 
-    overlay.appendChild(notificationWindow);
-
-    const buttonsContainer = document.createElement("div");
-    buttonsContainer.className = "buttons";
+    const buttonsContainer = $("<div>").addClass("buttons");
 
     for (const btnVal of buttonRoleList) {
-        const button = document.createElement("button");
-        button.classList.add("btn");
-        console.log(btnVal, Buttons.OK);
+        const button = $("<button>").addClass("btn");
+
         if (btnVal == Buttons.OK) {
-            button.classList.add("btn-outline-success", "mx-1");
-            button.role = Buttons.OK;
-            button.textContent = "OK";
+            button.addClass("btn-outline-success mx-1").attr("role", Buttons.OK).text("OK");
+        } else if (btnVal == Buttons.Cancel) {
+            button.addClass("btn-outline-danger mx-1").attr("role", Buttons.Cancel).text("Cancel");
         }
-        else if (btnVal == Buttons.Cancel) {
-            button.classList.add("btn-outline-danger", "mx-1");
-            button.role = Buttons.Cancel;
-            button.textContent = "Cancel";
-        }
-        button.addEventListener("click", closePopup);
-        buttonsContainer.appendChild(button);
+
+        button.on("click", closePopup);
+        buttonsContainer.append(button);
     }
-    notificationWindow.appendChild(buttonsContainer);
+    notificationWindow.append(buttonsContainer);
+    $(overlay).append(notificationWindow);
     setTimeout(function () {
         overlay.classList.remove("hidden");
-    }, 100)
+    }, 100);
     overlay.hidden = false;
     return notificationWindow;
 }
